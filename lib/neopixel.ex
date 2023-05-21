56d43e18478e97085f768e8b6f05ed853230551a
Joshua Miertschin <joshuamiertschin@gmail.com>
2023-05-21 13:38:27 -0500

    it works now
    Signed-off-by: Joshua Miertschin <joshuamiertschin@gmail.com>

--- lib/neopixel.ex
+++ lib/neopixel.ex
@@ -1,3 +1,66 @@
 defmodule Neopixel do
 
+  import Neopixel.Guards
+
+  @server Neopixel.Server
+
+  alias Neopixel.Server
+
+  def start_server(args) do
+    Server.start_link(args)
+  end
+
+  def set_pixel(strip, pixel, "#" <> hexcolor) when is_strip(strip) do
+    GenServer.call(@server, {:set_pixel, strip, pixel, hexcolor})
+  end
+
+  def set_pixel(strip, pixel, {r, g, b} = _color) when is_strip(strip) and is_rgb(r, g, b) do
+    hexcolor = Base.encode16(<<0, r, g, b>>)
+    GenServer.call(@server, {:set_pixel, strip, pixel, hexcolor})
+  end
+
+  def set_pixel(strip, pixel, {w, r, g, b} = _color)
+      when is_strip(strip) and is_wrgb(w, r, g, b) do
+    hexcolor = Base.encode16(<<w, r, g, b>>)
+    GenServer.call(@server, {:set_pixel, strip, pixel, hexcolor})
+  end
+
+  def fill_strip(strip, "#" <> hexcolor) when is_strip(strip) do
+    GenServer.call(@server, {:fill_strip, strip, hexcolor})
+  end
+
+  def fill_strip(strip, {r, g, b} = _color) when is_strip(strip) and is_rgb(r, g, b) do
+    hexcolor = Base.encode16(<<0, r, g, b>>)
+    GenServer.call(@server, {:fill_strip, strip, hexcolor})
+  end
+
+  def fill_strip(strip, {w, r, g, b} = _color) when is_strip(strip) and is_wrgb(w, r, g, b) do
+    hexcolor = Base.encode16(<<w, r, g, b>>)
+    GenServer.call(@server, {:fill_strip, strip, hexcolor})
+  end
+
+  def fill_rainbow(strip, hue_offset) when is_strip(strip) and is_255(hue_offset) do
+    GenServer.call(@server, {:fill_rainbow, strip, hue_offset})
+  end
+
+  def fill_hue(strip, "#" <> hsvcolor) when is_strip(strip) do
+    GenServer.call(@server, {:fill_hue, strip, hsvcolor})
+  end
+
+  def fill_hue(strip, hue_offset) when is_strip(strip) and is_255(hue_offset) do
+    hsvcolor = Base.encode16(<<0, hue_offset, 0xFF, 0xFF>>)
+    GenServer.call(@server, {:fill_hue, strip, hsvcolor})
+  end
+
+  def set_brightness(strip, brightness) when is_strip(strip) and is_255(brightness) do
+    GenServer.call(@server, {:set_brightness, strip, brightness})
+  end
+
+  def hsv_to_rgb("#" <> hexcolor) do
+    GenServer.call(@server, {:hsvrgb, hexcolor})
+  end
+
+  def render() do
+    GenServer.call(@server, :render)
+  end
 end
--- lib/neopixel/HAL.ex
+++ lib/neopixel/HAL.ex
@@ -1,72 +1,16 @@
 defmodule Neopixel.Server do
   @moduledoc """
   # Options:
-  * `:dma_channel` the dma channel the neopixel driver will use
+  * `:dma_channel` the dma channel the neopixel driver will use, default: 10
   * `:strip0_pin` pin number running strip 0
   * `:strip1_pin` pin number running strip 1
   * `:strip0_length` number of leds on strip 0
   * `:strip1_length` number of leds on strip 1
   """
 
-  import Neopixel.Guards
-
   use GenServer
 
   require Logger
-
-  def set_pixel(strip, pixel, "#" <> hexcolor) when is_strip(strip) do
-    GenServer.call(__MODULE__, {:set_pixel, strip, pixel, hexcolor})
-  end
-
-  def set_pixel(strip, pixel, {r, g, b} = _color) when is_strip(strip) and is_rgb(r, g, b) do
-    hexcolor = Base.encode16(<<0, r, g, b>>)
-    GenServer.call(__MODULE__, {:set_pixel, strip, pixel, hexcolor})
-  end
-
-  def set_pixel(strip, pixel, {w, r, g, b} = _color)
-      when is_strip(strip) and is_wrgb(w, r, g, b) do
-    hexcolor = Base.encode16(<<w, r, g, b>>)
-    GenServer.call(__MODULE__, {:set_pixel, strip, pixel, hexcolor})
-  end
-
-  def fill_strip(strip, "#" <> hexcolor) when is_strip(strip) do
-    GenServer.call(__MODULE__, {:fill_strip, strip, hexcolor})
-  end
-
-  def fill_strip(strip, {r, g, b} = _color) when is_strip(strip) and is_rgb(r, g, b) do
-    hexcolor = Base.encode16(<<0, r, g, b>>)
-    GenServer.call(__MODULE__, {:fill_strip, strip, hexcolor})
-  end
-
-  def fill_strip(strip, {w, r, g, b} = _color) when is_strip(strip) and is_wrgb(w, r, g, b) do
-    hexcolor = Base.encode16(<<w, r, g, b>>)
-    GenServer.call(__MODULE__, {:fill_strip, strip, hexcolor})
-  end
-
-  def fill_rainbow(strip, hue_offset) when is_strip(strip) and is_255(hue_offset) do
-    GenServer.call(__MODULE__, {:fill_rainbow, strip, hue_offset})
-  end
-
-  def fill_hue(strip, "#" <> hsvcolor) when is_strip(strip) do
-    GenServer.call(__MODULE__, {:fill_hue, strip, hsvcolor})
-  end
-
-  def fill_hue(strip, hue_offset) when is_strip(strip) and is_255(hue_offset) do
-    hsvcolor = Base.encode16(<<0, hue_offset, 0xFF, 0xFF>>)
-    GenServer.call(__MODULE__, {:fill_hue, strip, hsvcolor})
-  end
-
-  def set_brightness(strip, brightness) when is_strip(strip) and is_255(brightness) do
-    GenServer.call(__MODULE__, {:set_brightness, strip, brightness})
-  end
-
-  def hsv_to_rgb("#" <> hexcolor) do
-    GenServer.call(__MODULE__, {:hsvrgb, hexcolor})
-  end
-
-  def render() do
-    GenServer.call(__MODULE__, :render)
-  end
 
   # test function for sending commands calls directly to Neopixel
   @doc false
@@ -81,7 +25,7 @@ defmodule Neopixel.Server do
 
   @doc false
   def init(args) do
-    file = Application.app_dir(:neopixels, ["priv", "Neopixel"]) |> String.to_charlist()
+    file = Application.app_dir(:neopixel, ["priv", "Neopixel"]) |> String.to_charlist()
 
     config = [
       "#{args[:dma_channel] || 10}",
